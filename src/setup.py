31a7be2373f6fea7cd63afb5c7e60ac92c1addd6
Chris Loew <cloew123@gmail.com>
2015-03-03 14:34:42 -0700

    Added Boilerplate Package code

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='smart_defaults',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['smart_defaults'],
+     )
